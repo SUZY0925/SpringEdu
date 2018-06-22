a0b21c8dc396a846017e2148f1b37a91e7179c7e
SUZY0925 <38512853+SUZY0925@users.noreply.github.com>
2018-06-22 16:37:26 +0900

    180622 1637

--- src/main/java/com/kh/myapp/rbbs/dto/RbbsDTO.java
+++ src/main/java/com/kh/myapp/rbbs/dto/RbbsDTO.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Entity
@@ -11,5 +13,6 @@ import lombok.Data;
 public class RbbsDTO {
 	private int rnum, bnum, rgood, rbad, rgroup, rstep, rindent;
 	private String rid, rname, rcontent;
+	/*@JsonFormat(pattern="yyyy/MM/dd")*/
 	private Date rcdate, rudate;
 }
